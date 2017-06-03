e7b8a016a5d69e4156c8ac071cfc458a0211cfa4
Luke Chen Shui <chenshuiluke@gmail.com>
2017-06-03 13:45:53 -0500

    Didn't know the other test scripts had the same directory self check. Modified them too.

--- infoset/test/test_validate_checkdata.py
+++ infoset/test/test_validate_checkdata.py
@@ -11,7 +11,7 @@ import sys
 _TEST_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 _LIB_DIRECTORY = os.path.abspath(os.path.join(_TEST_DIRECTORY, os.pardir))
 _ROOT_DIRECTORY = os.path.abspath(os.path.join(_LIB_DIRECTORY, os.pardir))
-if _TEST_DIRECTORY.endswith('/infoset-ng/infoset/test') is True:
+if _TEST_DIRECTORY.endswith('/infoset/test') is True:
     sys.path.append(_ROOT_DIRECTORY)
 else:
     print(
