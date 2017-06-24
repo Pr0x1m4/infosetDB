46c296f0269588c5b2f63a0a3ec396232ab89b71
Luke Chen Shui <chenshuiluke@gmail.com>
2017-06-24 16:05:43 -0500

    Config page now shows current value of config variables

--- infoset/api/config_page.py
+++ infoset/api/config_page.py
@@ -3,7 +3,9 @@
 # Flask imports
 from flask import Blueprint
 from flask import render_template
+from infoset.utils import configuration
 import os
+
 # Define the STATUS global variable
 CONFIG_PAGE = Blueprint('CONFIG_PAGE', __name__)
 
@@ -20,4 +22,32 @@ def getConfig():
 
     """
     # Return
-    return render_template('config.html')
+    configDict = getCurrentConfigFileContents()
+    return render_template('config.html', config=configDict)
+
+def getCurrentConfigFileContents():
+    configDict = {}
+    config = configuration.Config()
+    configDict['infoset-username'] = config.username()
+    configDict['infoset-port'] = config.bind_port()
+
+    configDict['db-name'] = config.db_name()
+    configDict['db-host'] = config.db_hostname()
+    configDict['db-username'] = config.db_username()
+    configDict['db-password'] = config.db_password()
+
+    configDict['ingester-cache'] = config.ingest_cache_directory()
+    configDict['ingester-interval'] = config.interval()
+    configDict['ingester-pool-size'] = config.ingest_pool_size()
+    configDict['ingester-listen-address'] = config.listen_address()
+
+    configDict['log-directory'] = config.log_directory()
+    configDict['log-level'] = config.log_level()
+
+    configDict['memcached-host'] = config.memcached_hostname()
+    configDict['memcached-port'] = config.memcached_port()
+
+    configDict['sqlalchemy-overflow'] = config.sqlalchemy_max_overflow()
+    configDict['sqlalchemy-pool-size'] = config.sqlalchemy_pool_size()
+    return configDict
+
