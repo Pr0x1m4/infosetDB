34cc41095be2fc9bf5bbc35e87e4e32c559378d3
Jordan Jones <proxima.aust@gmail.com>
2017-07-30 11:58:16 -0500

    Added meta-data for setup

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
 from distutils.core import setup
 
+install_requires = [
+    "Flask",
+    "celery",
+    "setuptools",
+    "PyMySQL",
+    "PyYAML",
+    "requests",
+    "SQLAlchemy",
+    "SQLAlchemy-Utils",
+    "Werkzeug",
+    "redis"
+]
+
+tests_require = [
+    "mock",
+    "nose"
+]
+
 setup(
     name='Infoset DB',
     version='2.1.0',
+    author='Jordan Jones',
+    author_email='jordn_jones94@gmail.com',
+    maintainer="The Palisadoes Foundation",
+    maintainer_email="jordn_jones94@gmail.com",
+    url="https://github.com/PalisadoesFoundation/infoset-ng",
+    install_requires=install_requires,
+    tests_require=tests_require,
     packages=['infosetdb', ],
     license='Apache License 2',
 )
