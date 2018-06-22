2c2807c7549085df2e5d6a63a8563c10061557a6
Jhamali <dubzero2221996@hotmail.com>
2018-06-21 19:30:48 -0500

    Creates GraphQL Schema for Agent (#161)
    * Add GraphQL scehma for agent
    
    * Update schema_agent.py
    
    * Update schema_agent.py

---
+++ infoset/api/schema_agent.py
@@ -0,0 +1,67 @@
+import graphene
+from graphene import relay
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from infoset.api import graphene_utils
+from infoset.db.db_orm import db_session, Agent as AgentModel
+from datetime import datetime
+
+class AgentAttribute:    
+
+    idx_agent =  graphene.ID(description="")
+    idx_agentname = graphene.Float(description="")
+    id_agent = graphene.String(description="")
+    enabled = graphene.Float(description="")
+    ts_modified = graphene.DateTime(description="")
+    ts_created = graphene.DateTime(description="")
+
+
+class Agent(SQLAlchemyObjectType, AgentAttribute):
+    class Meta:
+        model = AgentModel
+        interfaces = (relay.Node, )
+
+
+class AgentInput(graphene.InputObjectType, AgentAttribute):
+    """Arguments to create Agent."""
+    pass
+
+
+class CreateAgent(graphene.Mutation):
+    """Mutation to create a Agent."""
+    _agent = graphene.Field(
+        lambda: Agent, description="Agent created by this mutation.")
+
+    class Arguments:
+        input = AgentInput(required=True)
+
+    def mutate(self, info, input):
+        data = graphene_utils.input_to_dictionary(input)
+        data['ts_created'] = datetime.utcnow()
+        data['ts_modified'] = datetime.utcnow()
+
+        _agent = AgentModel(**data)
+        db_session.add(_agent)
+        db_session.commit()
+
+        return CreateAgent(_agent=_agent)
+
+class UpdateAgentInput(graphene.InputObjectType, AgentAttribute):
+
+    _agent = graphene.ID(required=True, description="Unique identifier of the Agent")
+    
+
+class UpdateAgent(graphene.Mutation):
+    _agent = graphene.Field(lambda: Agent, description="Agent updated by this mutation.")
+
+    class Arguments:
+        input = UpdateAgentInput(required=True)
+
+    def mutate(self, info, input):
+        data = graphene_utils.input_to_dictionary(input)
+        data['ts_modified'] = datetime.utcnow()
+        _agent = db_session.query(AgentModel).filter_by(id=data['id'])
+        _agent.update(data)
+        db_session.commit()
+        _agent = db_session.query(AgentModel).filter_by(id=data['id']).first()
+
+        return UpdateAgent(_agent=_agent)
