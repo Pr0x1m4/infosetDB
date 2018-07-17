847a9d26a4e65bc1fee28c6e8b725f1044872002
Jordan Jones <proxima.aust@gmail.com>
2018-07-17 10:52:49 -0500

    Update setup with DB initialization

--- setup.py
+++ setup.py
@@ -1,39 +1,18 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-from maintenance.database import DatabaseSetup
-
-install_requires = [
-    "Flask",
-    "celery",
-    "setuptools",
-    "PyMySQL",
-    "PyYAML",
-    "requests",
-    "SQLAlchemy",
-    "SQLAlchemy-Utils",
-    "Werkzeug",
-    "redis"
-]
-
-tests_require = [
-    "mock",
-    "nose"
-]
+from setup.database import DatabaseSetup
 
 setup(
     name='Infoset DB',
     version='2.1.1',
     author='Jordan Jones',
-    author_email='jordn_jones94@gmail.com',
+    author_email='proxima.aust@gmail.com',
     maintainer="The Palisadoes Foundation",
-    maintainer_email="jordn_jones94@gmail.com",
+    maintainer_email="proxima.aust@gmail.com",
     url="https://github.com/PalisadoesFoundation/infoset-ng",
-    install_requires=install_requires,
-    tests_require=tests_require,
-    packages=find_packages('.'),
     license='Apache License 2',
     cmdclass={
-        'install': DatabaseSetup,
+        'db': DatabaseSetup,
     },
 )
